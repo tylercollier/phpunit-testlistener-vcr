5ee30d648d40781a79ae1895e5e96f81892b83f1
Tyler Collier <git@tylercollier.com>
2014-04-26 23:50:12 -0700

    Added test for cassette path

--- tests/PHPUnit/Util/Log/VCRTest.php
+++ tests/PHPUnit/Util/Log/VCRTest.php
@@ -17,10 +17,20 @@ class Util_Log_VCRTest extends PHPUnit_Framework_TestCase
     /**
      * @vcr unittest_annotation_test
      */
-    public function testInterceptsWithAnnotations()
+    public function xtestInterceptsWithAnnotations()
     {
         // Content of tests/fixtures/unittest_annotation_test: "This is a annotation test dummy".
         $result = file_get_contents('http://google.com');
         $this->assertEquals('This is a annotation test dummy.', $result, 'Call was not intercepted (using annotations).');
+    }
+
+    /**
+     * @vcr unittest_annotation_test
+     */
+    public function testCan()
+    {
+        // Content of tests/fixtures_alt/unittest_annotation_test: "This is an alternate annotation test dummy".
+        $result = file_get_contents('http://google.com');
+        $this->assertEquals('This is an alternative annotation test dummy.', $result, 'Call was not intercepted (using annotations).');
     }
 }
